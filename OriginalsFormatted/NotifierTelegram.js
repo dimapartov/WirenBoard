7b78e8e5989e0346ce74dc81a5cd162a6e8dab33
dimapartov <mr_kubarev@mail.ru>
2024-07-05 21:56:01 +0300

    Cleanup code

--- OriginalsFormatted/NotifierTelegram.js
+++ OriginalsFormatted/NotifierTelegram.js
@@ -1,4 +1,4 @@
-var postMsg = "curl -s -X POST https://api.telegram.org/bot5175995984:AAFbasn2TirtYsW4BiCuCwB5tmbe6wiwGko/sendMessage -d chat_id='{}' -d text='{}'";
+var postMsg = "curl -s -X POST https://api.telegram.org/bot_token_here/sendMessage -d chat_id='{}' -d text='{}'";
 var users = [226353952, 770592984];
 var admins = [254556808, 104577570];
 global.__proto__.SendTelegramMsg = function (priority, msg) {
