8d060aebb7bdcf5c42145c872fec74866741385c
dimapartov <mr_kubarev@mail.ru>
2026-08-07 13:58:44 +0600

    hotfix

--- Tests/HourlyProwlNotificationTest.js
+++
@@ -1,8 +0,0 @@
-var Prowl = require("ProwlModule");
-defineRule("hourly_prowl_notification", {
-//    when: cron("0 0 * * * *"),
-    when: cron("0 15 10-22 * * *"),
-    then: function () {
-        Prowl.send("Тест!", "Каждый час текст тестового уведомления провл", 0);
-    }
-});
